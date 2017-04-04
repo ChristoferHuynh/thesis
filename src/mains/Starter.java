26e6b09d2326434e40958f9355054b9b4af9e5e8
JesperGustafsson <jesper95gustafsson@gmail.com>
2017-04-04 08:54:34 +0200

    hehe lite grejer

--- src/mains/Starter.java
+++ src/mains/Starter.java
@@ -1,6 +1,8 @@
 package mains;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 
 import parsers.RJParser;
@@ -9,22 +11,49 @@ public class Starter {
 
 	public static void main(String[] args) {
 		
-		HashMap<String, String> customerValues = new HashMap<String, String>();
-		HashMap<String, String> secureValues = new HashMap<String, String>();
+		HashMap<String, String[]> processes = new HashMap<String, String[]>();
+
+		// Should be decided when running the file.
+		File resultDir;
+		String hostname;
+		String test_name;
+		String resultPath;
 		
-		File customerFile;
-		File secureFile;
+		File processesFile;
 		
-		//Kan kolla filnamn för att se från vilken audit det är ifrån men for now..
+		hostname = "jesper-Aspire-E5-571";
+		test_name = "encrypted_disk_info";		
+		resultPath = "C:\\\\Users\\\\Jesper\\\\Downloads\\\\result\\\\result\\\\";
+		resultDir = new File("C:\\\\Users\\\\Jesper\\\\Downloads\\\\result\\\\result");
+		
+		processesFile = new File(resultPath
+				+ test_name + "\\\\" + hostname + ".log");
 		
 		
-		//remote-job
 		RJParser rjParser = new RJParser();
-		customerFile = new File(""); // < path
-		secureFile = new File(""); 	 // < path
 		
-		customerValues = rjParser.read(customerFile);
-		secureValues = rjParser.read(secureFile);
+
+		try {
+			Method method = rjParser.getClass().getMethod("read_" + test_name, File.class);
+			processes = (HashMap<String, String[]>) method.invoke(rjParser, processesFile);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
+		
+//		processes = rjParser.readProcesses(processesFile);
+		
+		System.out.println(processes.get("21"));
+		String[] processVector = processes.get("21");
+		for (int i = 0; i < processVector.length; i++) {
+			System.out.print(processVector[i] + "\t");
+		}
+		System.out.println("\nDONE");
+		
+		
 		
 	}
 }
--- src/parsers/RJParser.java
+++ src/parsers/RJParser.java
@@ -20,6 +20,8 @@ public class RJParser extends Parser {
 		while (scanner.hasNext()) {
 			String nextKey = scanner.next(); // key = value i loggsen.. behöver nog göras på annorlunda sätt men du fattar
 			String nextValue = scanner.next();
+			System.out.println(nextKey);
+			System.out.println(nextValue);
 			values.put(nextKey, nextValue);
 		}
 		
@@ -32,4 +34,116 @@ public class RJParser extends Parser {
 		return false;
 	}
 
+	public HashMap<String, String[]> read_processes_info(File customerFile) {
+		HashMap<String, String[]> values = new HashMap<String, String[]>(); 
+		
+		try {
+			scanner = new Scanner(customerFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while (scanner.hasNext()) {
+			String[] nextValue = new String[11];
+			for (int i = 0; i < nextValue.length - 1; i++) {
+				nextValue[i] = scanner.next();
+			}
+			nextValue[10] = scanner.nextLine();
+			values.put(nextValue[1], nextValue);
+		}
+		
+		return values;
+	}
+	
+	public HashMap<String, String[]> read_cron_at_info(File customerFile) { //Unsure how to parse, how does it look when directory exists?
+		HashMap<String, String[]> values = new HashMap<String, String[]>(); 
+		int id = 1;
+		String PID = Integer.toString(id);
+		
+		try {
+			scanner = new Scanner(customerFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while (scanner.hasNext()) {
+			String[] nextValue = new String[1];
+			nextValue[0] = scanner.nextLine();
+			PID = Integer.toString(id);			
+			values.put(PID, nextValue);
+			id++;
+		}
+		
+		return values;
+	}
+
+	
+	public HashMap<String, String[]> read_crontab_info(File customerFile) { //Unsure how to parse, how does it look when they exist?
+		HashMap<String, String[]> values = new HashMap<String, String[]>(); 
+		
+		int id = 1;
+		String PID = Integer.toString(id);
+		
+		try {
+			scanner = new Scanner(customerFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while (scanner.hasNext()) {
+			String[] nextValue = new String[4];
+			for (int i = 0; i < nextValue.length; i++) {
+				nextValue[i] = scanner.next();
+			}
+			nextValue[10] = scanner.nextLine();
+			
+			PID = Integer.toString(id);			
+			values.put(PID, nextValue);
+			id++;
+		}
+		
+		return values;
+	}
+	
+	public HashMap<String, String[]> read_disvolume_info(File customerFile) {
+		HashMap<String, String[]> values = new HashMap<String, String[]>(); 
+		
+		try {
+			scanner = new Scanner(customerFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while (scanner.hasNext()) {
+			String[] nextValue = new String[6];
+			for (int i = 0; i < nextValue.length - 1; i++) {
+				nextValue[i] = scanner.next();
+			}
+			nextValue[10] = scanner.nextLine();
+			values.put(nextValue[1], nextValue);
+		}
+		
+		return values;
+	}
+
+	
+	public HashMap<String, String[]> read_encrypted_disk_info(File customerFile) {
+		HashMap<String, String[]> values = new HashMap<String, String[]>(); 
+		String PID;
+		String PTUUID;
+		try {
+			scanner = new Scanner(customerFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while (scanner.hasNext()) {
+			PID = scanner.next();
+			PTUUID = scanner.findInLine("PTUasdasd");
+			System.out.println(PID + "\t" + PTUUID);
+		}
+		
+		return values;
+	}
+	
 }
